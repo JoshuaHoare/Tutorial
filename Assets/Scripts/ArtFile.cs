b8e9e7e70cf0f0cca6a02bb30fb792ea89399990
iMPhox <joshuahoare3d@gmail.com>
2021-08-19 22:44:17 +1000

    custom editor setup, asset file created

---
+++ Assets/Scripts/ArtFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ArtFile : MonoBehaviour
+{
+    [SerializeField] RectTransform rect;
+
+    public void Yeet()
+    {
+        Debug.Log("test");
+    }
+
+}
---
+++ Assets/Scripts/CustomInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ArtFile))]
+public class CustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("3D Model Properties");
+        DrawDefaultInspector();
+
+        ArtFile Component = (ArtFile)target;
+        if (GUILayout.Button("Editor Update"))
+            Component.Yeet();
+    }
+}
