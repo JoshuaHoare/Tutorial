89f8c41d669c11d9cbda668a20884bccb186aac3
iMPhox <joshuahoare3d@gmail.com>
2021-10-13 20:52:53 +1000

    Setting up the chunk prefab

--- Assets/Scripts/ProcGenerator/Chunks/Chunk.cs
+++ Assets/Scripts/ProcGenerator/Chunks/Chunk.cs
@@ -2,17 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class VerticeProperties
+{
+    public Color32 color;
+    public Vector3 vector;
+    public int index;
+
+    public VerticeProperties(Color32 color, Vector3 vector, int index)
+    {
+        this.color = color;
+        this.vector = vector;
+        this.index = index;
+    }
+}
+
+[ExecuteInEditMode]
 public class Chunk : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Chunk Components")]
+    [SerializeField] Mesh mesh;
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] MeshRenderer meshRenderer;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Seams")]
+    [SerializeField] List<List<Vector3>> Seams;
+    [SerializeField] List<VerticeProperties> vertProperties;
+    [SerializeField] List<Vector3> testSeam;
+    [SerializeField] List<Color32> testColors;
+    [SerializeField] List<Vector3> sortedVerts;
+
+    #region Debug
+    public void SetupVertProperties()
     {
-        
+        Debug.Log("Setting Up Vertex Properties");
+        testColors.Clear();
+        mesh = meshFilter.sharedMesh;
+        testColors.AddRange(mesh.colors32);
+        List<Color32> sortedColors = new List<Color32>();
+        float alphaCase = testColors[4].a;
+        int i = 0;
+        foreach (Color32 color in testColors)
+        {
+            if (color.a != 0)
+            {
+                vertProperties.Add(new VerticeProperties(color, mesh.vertices[i], i));
+            }
+            i++;
+        }
+    }
+    #endregion
+
+    public void CalculateVertexData()
+    {
+
     }
 }
---
+++ Assets/Scripts/ProcGenerator/Chunks/ChunkCustomInspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Chunk))]
+public class ChunkCustomInspector : Editor
+{
+    bool setup = false;
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("3D Model Properties");
+        DrawDefaultInspector();
+
+        Chunk chunk = (Chunk)target;
+        if (GUILayout.Button("Setup Vert Properties"))
+        {
+            setup = true;
+            chunk.SetupVertProperties();
+        }
+        if (GUILayout.Button("Reload") && setup)
+            chunk.CalculateVertexData();
+    }
+}
